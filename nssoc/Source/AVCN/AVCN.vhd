0b411690a8a49d3410b6e387aef5838b523dda9e
Daniel Gutierrez Galan <dgutierrez@atc.us.es>
2021-01-20 18:20:45 +0100

    Added copyright and comments

--- nssoc/Source/AVCN/AVCN.vhd
+++ nssoc/Source/AVCN/AVCN.vhd
@@ -1,84 +1,115 @@
-----------------------------------------------------------------------------------
--- Company: University of Sevilla
--- Engineer: Daniel Gutierrez-Galan
--- 
--- Create Date: 12.12.2018 11:57:45
--- Design Name: 
--- Module Name: AVCN - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
+--/////////////////////////////////////////////////////////////////////////////////
+--//                                                                             //
+--//    Copyright (c) 2020  Daniel Gutierrez Galan                               //
+--//                                                                             //
+--//    This file is part of NSSOC.                                              //
+--//                                                                             //
+--//    NSSOC is free software: you can redistribute it and/or modify            //
+--//    it under the terms of the GNU General Public License as published by     //
+--//    the Free Software Foundation, either version 3 of the License, or        //
+--//    (at your option) any later version.                                      //
+--//                                                                             //
+--//    NSSOC is distributed in the hope that it will be useful,                 //
+--//    but WITHOUT ANY WARRANTY; without even the implied warranty of           //
+--//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the              //
+--//    GNU General Public License for more details.                             //
+--//                                                                             //
+--//    You should have received a copy of the GNU General Public License        //
+--//    along with NSSOC. If not, see <http://www.gnu.org/licenses/>.            //
+--//                                                                             //
+--/////////////////////////////////////////////////////////////////////////////////
+
+-------------------------------------------------------------------------------
+-- Title      : Antero ventricular coclear nucleus
+-- Project    : NSSOC
+-------------------------------------------------------------------------------
+-- File       : AVCN.vhd
+-- Author     : Daniel Gutierrez-Galan (dgutierrez@atc.us.es)
+-- Company    : University of Seville
+-- Created    : 2018-12-12
+-- Last update: 2021-01-13
+-- Platform   : any
+-- Standard   : VHDL'93/02
+-------------------------------------------------------------------------------
+-- Description: Main module
+-------------------------------------------------------------------------------
+-- Copyright (c) 2020 
+-------------------------------------------------------------------------------
+-- Revisions  :
+-- Date        Version  Author  Description
+-- 2020-01-20  1.0      dgutierrez	Created
+-------------------------------------------------------------------------------
+
+-------------------------------------------------------------------------------
+-- Libraries
+-------------------------------------------------------------------------------
 LIBRARY IEEE;
 USE IEEE.STD_LOGIC_1164.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
+-------------------------------------------------------------------------------
+-- Entity declaration
+-------------------------------------------------------------------------------
 ENTITY AVCN IS
     GENERIC (
-        NUM_FREQ_CH : INTEGER := 36
+        NUM_FREQ_CH : INTEGER := 64
     );
     PORT (
-        i_clock : IN std_logic;
-        i_nreset : IN std_logic;
-        i_auditory_nerve_spikes : IN std_logic_vector(((NUM_FREQ_CH * 2) - 1) DOWNTO 0);
-        o_phase_locked_spikes : OUT std_logic_vector((NUM_FREQ_CH - 1) DOWNTO 0)
+        i_clock                 : IN  std_logic;
+        i_nreset                : IN  std_logic;
+        i_auditory_nerve_spikes : IN  std_logic_vector(((NUM_FREQ_CH * 2) - 1) DOWNTO 0);
+        o_phase_locked_spikes   : OUT std_logic_vector((NUM_FREQ_CH - 1) DOWNTO 0)
     );
 END AVCN;
 
+-------------------------------------------------------------------------------
+-- Architecture
+-------------------------------------------------------------------------------
 ARCHITECTURE Behavioral OF AVCN IS
 
-    --====================================================--
-    -- Components
-    --====================================================--
+    ---------------------------------------------------------------------------
+    -- Constants declaration
+    ---------------------------------------------------------------------------
+
+    ---------------------------------------------------------------------------
+    -- Signals declaration
+    ---------------------------------------------------------------------------
+
+
+    ---------------------------------------------------------------------------
+    -- Components declaration
+    ---------------------------------------------------------------------------
+    
+    --
     --Spherical bushy cell neuron component
+    --
     COMPONENT spherical_bushy_cell IS
         PORT (
-            i_clock : IN STD_LOGIC;
-            i_nreset : IN STD_LOGIC;
-            i_pos_spike : IN STD_LOGIC;
-            i_neg_spike : IN STD_LOGIC;
+            i_clock              : IN  STD_LOGIC;
+            i_nreset             : IN  STD_LOGIC;
+            i_pos_spike          : IN  STD_LOGIC;
+            i_neg_spike          : IN  STD_LOGIC;
             o_phase_locked_spike : OUT STD_LOGIC
         );
     END COMPONENT;
 
-    --====================================================--
-    -- Constants
-    --====================================================--
+BEGIN  -- architecture Behavioral
 
-    --====================================================--
-    -- Signals
-    --====================================================--
-BEGIN
+    ---------------------------------------------------------------------------
+    -- Components instantiation
+    ---------------------------------------------------------------------------
 
-    --====================================================--
-    -- Instances
-    --====================================================--
-
-    GEN_SBC :
-    FOR I IN 0 TO (NUM_FREQ_CH - 1) GENERATE
+    --
+    -- Spherical bushy cell
+    --
+    GEN_SBC : FOR I IN 0 TO (NUM_FREQ_CH - 1) GENERATE
         SBCX : spherical_bushy_cell
-        PORT MAP(
-            i_clock => i_clock,
-            i_nreset => i_nreset,
-            i_pos_spike => i_auditory_nerve_spikes(I * 2),
-            i_neg_spike => i_auditory_nerve_spikes((I * 2) + 1),
-            o_phase_locked_spike => o_phase_locked_spikes(I)
-        );
+            PORT MAP(
+                i_clock              => i_clock,
+                i_nreset             => i_nreset,
+                i_pos_spike          => i_auditory_nerve_spikes(I * 2),
+                i_neg_spike          => i_auditory_nerve_spikes((I * 2) + 1),
+                o_phase_locked_spike => o_phase_locked_spikes(I)
+            );
     END GENERATE GEN_SBC;
 
 END Behavioral;
--- nssoc/Source/AVCN/spherical_bushy_cell.vhd
+++ nssoc/Source/AVCN/spherical_bushy_cell.vhd
@@ -1,83 +1,124 @@
-----------------------------------------------------------------------------------
--- Company: University of Sevilla
--- Engineer: Daniel Gutierrez-Galan
--- 
--- Create Date: 12.12.2018 11:57:45
--- Design Name: 
--- Module Name: s_bushy_cell - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
+--/////////////////////////////////////////////////////////////////////////////////
+--//                                                                             //
+--//    Copyright (c) 2020  Daniel Gutierrez Galan                               //
+--//                                                                             //
+--//    This file is part of NSSOC project.                                      //
+--//                                                                             //
+--//    NSSOC is free software: you can redistribute it and/or modify            //
+--//    it under the terms of the GNU General Public License as published by     //
+--//    the Free Software Foundation, either version 3 of the License, or        //
+--//    (at your option) any later version.                                      //
+--//                                                                             //
+--//    NSSOC is distributed in the hope that it will be useful,                 //
+--//    but WITHOUT ANY WARRANTY; without even the implied warranty of           //
+--//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the              //
+--//    GNU General Public License for more details.                             //
+--//                                                                             //
+--//    You should have received a copy of the GNU General Public License        //
+--//    along with NSSOC. If not, see <http://www.gnu.org/licenses/>.            //
+--//                                                                             //
+--/////////////////////////////////////////////////////////////////////////////////
+
+-------------------------------------------------------------------------------
+-- Title      : Spherical bushy cell of the AVCN
+-- Project    : NSSOC
+-------------------------------------------------------------------------------
+-- File       : spherical_bushy_cell.vhd
+-- Author     : Daniel Gutierrez-Galan (dgutierrez@atc.us.es)
+-- Company    : University of Seville
+-- Created    : 2018-12-12
+-- Last update: 2021-01-13
+-- Platform   : any
+-- Standard   : VHDL'93/02
+-------------------------------------------------------------------------------
+-- Description: Main module
+-------------------------------------------------------------------------------
+-- Copyright (c) 2020 
+-------------------------------------------------------------------------------
+-- Revisions  :
+-- Date        Version  Author  Description
+-- 2020-01-20  1.0      dgutierrez	Created
+-------------------------------------------------------------------------------
+
+-------------------------------------------------------------------------------
+-- Libraries
+-------------------------------------------------------------------------------
 LIBRARY IEEE;
 USE IEEE.STD_LOGIC_1164.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
+-------------------------------------------------------------------------------
+-- Entity declaration
+-------------------------------------------------------------------------------
 ENTITY spherical_bushy_cell IS
     PORT (
-        i_clock : IN std_logic;
-        i_nreset : IN std_logic;
-        i_pos_spike : IN std_logic;
-        i_neg_spike : IN std_logic;
+        i_clock              : IN  std_logic;
+        i_nreset             : IN  std_logic;
+        i_pos_spike          : IN  std_logic;
+        i_neg_spike          : IN  std_logic;
         o_phase_locked_spike : OUT std_logic
     );
 END spherical_bushy_cell;
 
+-------------------------------------------------------------------------------
+-- Architecture
+-------------------------------------------------------------------------------
 ARCHITECTURE Behavioral OF spherical_bushy_cell IS
+    ---------------------------------------------------------------------------
+    -- Constants declaration
+    ---------------------------------------------------------------------------
 
-    --====================================================--
-    -- Signals
-    --====================================================--
-    SIGNAL last_spike_polarity : std_logic;
-    SIGNAL zero_cross : std_logic;
+    ---------------------------------------------------------------------------
+    -- Signals declaration
+    ---------------------------------------------------------------------------
+    SIGNAL r_last_spike_polarity : std_logic;
+    SIGNAL r_zero_cross          : std_logic;
 
-BEGIN
+    ---------------------------------------------------------------------------
+    -- Components declaration
+    ---------------------------------------------------------------------------
 
-    zero_crossing_detection : PROCESS (i_clock, i_nreset)--, last_spike_polarity, i_neg_spike)
+BEGIN  -- architecture Behavioral
+
+    -----------------------------------------------------------------------------
+    -- Processes
+    -----------------------------------------------------------------------------
+
+    -- purpose: Zero crossing detection
+    -- type   : sequential
+    -- inputs : i_clock, i_nreset
+    -- outputs: r_zero_cross
+    p_zero_crossing_detection : PROCESS (i_clock, i_nreset)
     BEGIN
         IF i_nreset = '0' THEN
-            zero_cross <= '0';
+            r_zero_cross <= '0';
         ELSE
             IF rising_edge(i_clock) THEN
-                IF (i_neg_spike = '1') AND (last_spike_polarity = '1') THEN
-                    zero_cross <= '1';
-                    --elsif (i_pos_spike = '1') and (last_spike_polarity = "10") then
-                    --zero_cross <= '1';
+                IF (i_neg_spike = '1') AND (r_last_spike_polarity = '1') THEN
+                    r_zero_cross <= '1';
                 ELSE
-                    zero_cross <= '0';
+                    r_zero_cross <= '0';
                 END IF;
             ELSE
 
             END IF;
         END IF;
 
-    END PROCESS zero_crossing_detection;
+    END PROCESS p_zero_crossing_detection;
 
-    spike_polarity_update : PROCESS (i_clock, i_nreset)--, i_pos_spike)--, i_neg_spike)
+    -- purpose: Store the polarity of the last event
+    -- type   : sequential
+    -- inputs : i_clock, i_nreset
+    -- outputs: r_last_spike_polarity
+    spike_polarity_update : PROCESS (i_clock, i_nreset)
     BEGIN
         IF i_nreset = '0' THEN
-            last_spike_polarity <= '0';
+            r_last_spike_polarity <= '0';
         ELSE
             IF rising_edge(i_clock) THEN
                 IF i_pos_spike = '1' THEN
-                    last_spike_polarity <= '1';
+                    r_last_spike_polarity <= '1';
                 ELSIF i_neg_spike = '1' THEN
-                    last_spike_polarity <= '0';
+                    r_last_spike_polarity <= '0';
                 ELSE
 
                 END IF;
@@ -87,7 +128,9 @@ BEGIN
         END IF;
     END PROCESS spike_polarity_update;
 
-    o_phase_locked_spike <= zero_cross;
-    --o_phase_locked_spike <= i_pos_spike OR i_neg_spike;
+    -----------------------------------------------------------------------------
+    -- Output assign
+    -----------------------------------------------------------------------------
+    o_phase_locked_spike <= r_zero_cross;
 
 END Behavioral;
