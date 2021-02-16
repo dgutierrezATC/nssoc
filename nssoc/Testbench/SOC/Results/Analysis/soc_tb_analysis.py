54eb4adb0e6674c5fc864e04ba99fae0df84df2e
Daniel Gutierrez Galan <dgutierrez@atc.us.es>
2021-02-16 22:19:20 +0100

    Updated file headers

--- nssoc/Testbench/SOC/Results/Analysis/soc_tb_analysis.py
+++ nssoc/Testbench/SOC/Results/Analysis/soc_tb_analysis.py
@@ -1,3 +1,30 @@
+"""
+--/////////////////////////////////////////////////////////////////////////////////
+--//                                                                             //
+--//    Copyright (c) 2020  Daniel Gutierrez Galan                               //
+--//                                                                             //
+--//    This file is part of NSSOC project.                                      //
+--//                                                                             //
+--//    NSSOC is free software: you can redistribute it and/or modify            //
+--//    it under the terms of the GNU General Public License as published by     //
+--//    the Free Software Foundation, either version 3 of the License, or        //
+--//    (at your option) any later version.                                      //
+--//                                                                             //
+--//    NSSOC is distributed in the hope that it will be useful,                 //
+--//    but WITHOUT ANY WARRANTY; without even the implied warranty of           //
+--//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the              //
+--//    GNU General Public License for more details.                             //
+--//                                                                             //
+--//    You should have received a copy of the GNU General Public License        //
+--//    along with NSSOC. If not, see <http://www.gnu.org/licenses/>.            //
+--//                                                                             //
+--/////////////////////////////////////////////////////////////////////////////////
+"""
+
+################################################################################
+# Imports
+################################################################################
+
 # Loading .aedat with localization information
 print("Starting the test proces...")
 settings = MainSettings(num_channels=64, mono_stereo=1, on_off_both=1, address_size=4, ts_tick=1, bin_size=20000)
