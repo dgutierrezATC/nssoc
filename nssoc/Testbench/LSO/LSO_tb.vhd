1a3d0583d53881fe63d799069d09a35142801595
Daniel Gutierrez Galan <dgutierrez@atc.us.es>
2021-02-17 10:52:41 +0100

    Cleaning the code

--- nssoc/Testbench/LSO/LSO_tb.vhd
+++ nssoc/Testbench/LSO/LSO_tb.vhd
@@ -1,86 +1,125 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 30.04.2019 22:26:23
--- Design Name: 
--- Module Name: LSO_tb - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
+--/////////////////////////////////////////////////////////////////////////////////
+--//                                                                             //
+--//    Copyright (c) 2020  Daniel Gutierrez Galan                               //
+--//                                                                             //
+--//    This file is part of NSSOC project.                                      //
+--//                                                                             //
+--//    NSSOC is free software: you can redistribute it and/or modify            //
+--//    it under the terms of the GNU General Public License as published by     //
+--//    the Free Software Foundation, either version 3 of the License, or        //
+--//    (at your option) any later version.                                      //
+--//                                                                             //
+--//    NSSOC is distributed in the hope that it will be useful,                 //
+--//    but WITHOUT ANY WARRANTY; without even the implied warranty of           //
+--//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the              //
+--//    GNU General Public License for more details.                             //
+--//                                                                             //
+--//    You should have received a copy of the GNU General Public License        //
+--//    along with NSSOC. If not, see <http://www.gnu.org/licenses/>.            //
+--//                                                                             //
+--/////////////////////////////////////////////////////////////////////////////////
+
+-------------------------------------------------------------------------------
+-- Title      : Testbench for the Lateral Superior Olivar (LSO) model
+-- Project    : NSSOC
+-------------------------------------------------------------------------------
+-- File       : LSO_tb.vhd
+-- Author     : Daniel Gutierrez-Galan (dgutierrez@atc.us.es)
+-- Company    : University of Seville
+-- Created    : 2019-04-30
+-- Last update: 2021-01-21
+-- Platform   : any
+-- Standard   : VHDL'93/02
+-------------------------------------------------------------------------------
 -- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
+-------------------------------------------------------------------------------
+-- Copyright (c) 2020 
+-------------------------------------------------------------------------------
+-- Revisions  :
+-- Date        Version  Author  Description
+-- 2020-01-20  1.0      dgutierrez	Created
+-------------------------------------------------------------------------------
+
+-------------------------------------------------------------------------------
+-- Libraries
+-------------------------------------------------------------------------------
 LIBRARY IEEE;
 USE IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---USE IEEE.NUMERIC_STD.ALL;
-use IEEE.STD_LOGIC_ARITH.ALL;
-use IEEE.STD_LOGIC_UNSIGNED.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
+USE IEEE.STD_LOGIC_ARITH.ALL;
+USE IEEE.STD_LOGIC_UNSIGNED.ALL;
 USE STD.TEXTIO.ALL;
 USE IEEE.STD_LOGIC_TEXTIO.ALL;
 
+-------------------------------------------------------------------------------
+-- Entity declaration
+-------------------------------------------------------------------------------
 ENTITY LSO_tb IS
-    --  Port ( );
+
 END LSO_tb;
 
+-------------------------------------------------------------------------------
+-- Architectures
+-------------------------------------------------------------------------------
 ARCHITECTURE Behavioral OF LSO_tb IS
 
+	-------------------------------------------------------------------------------
+    -- Component declaration for the unit under test (UUT)
+    -------------------------------------------------------------------------------
     COMPONENT LSO IS
         GENERIC (
-            NUM_FREQ_CH : INTEGER := 5
+            NUM_FREQ_CH     : INTEGER := 5;
+            NUM_NET_NEURONS : INTEGER := 1
         );
         PORT (
-            i_clock : IN std_logic;
-            i_reset : IN std_logic;
-            i_left_avcn_spikes : IN std_logic_vector(((NUM_FREQ_CH * 2) - 1) DOWNTO 0);
-            i_right_avcn_spikes : IN std_logic_vector(((NUM_FREQ_CH * 2) - 1) DOWNTO 0);
-            o_ild_out_spikes : OUT std_logic_vector(((NUM_FREQ_CH * 2) - 1) DOWNTO 0)
+            i_clock             : IN  STD_LOGIC;
+            i_nreset            : IN  STD_LOGIC;
+            i_left_avcn_spikes  : IN  STD_LOGIC_VECTOR(((NUM_FREQ_CH * 2) - 1) DOWNTO 0);
+            i_right_avcn_spikes : IN  STD_LOGIC_VECTOR(((NUM_FREQ_CH * 2) - 1) DOWNTO 0);
+            o_ild_out_spikes    : OUT STD_LOGIC_VECTOR(((NUM_FREQ_CH * NUM_NET_NEURONS * 2) - 1) DOWNTO 0)
         );
     END COMPONENT;
 
-    --Constants
+    ---------------------------------------------------------------------------
+    -- UUT signals declaration
+	---------------------------------------------------------------------------
+
+    -- Component constants
+    CONSTANT c_NUM_FREQ_CH     : INTEGER := 2;
+    CONSTANT c_NUM_NET_NEURONS : INTEGER := 1;
+
+    -- Component input ports
+    SIGNAL i_clock             : STD_LOGIC := '0';
+    SIGNAL i_nreset            : STD_LOGIC := '0';
+    SIGNAL i_left_avcn_spikes  : STD_LOGIC_VECTOR(((c_NUM_FREQ_CH * 2) - 1) DOWNTO 0) := (OTHERS => '0');
+    SIGNAL i_right_avcn_spikes : STD_LOGIC_VECTOR(((c_NUM_FREQ_CH * 2) - 1) DOWNTO 0) := (OTHERS => '0');
+
+    -- Component output ports
+    SIGNAL o_ild_out_spikes : STD_LOGIC_VECTOR(((c_NUM_FREQ_CH * c_NUM_NET_NEURONS * 2) - 1) DOWNTO 0);
+
+	---------------------------------------------------------------------------
+    -- Testbench signals declaration
+    ---------------------------------------------------------------------------
+
+    -- Clock
     CONSTANT c_i_clock_period : TIME := 20 ns;
 
-    CONSTANT c_NUM_FREQ_CH : INTEGER := 2;
+   
 
     CONSTANT c_START_FREQ_CH : INTEGER := 24;
-    
+
     --Testbench parameters
     CONSTANT c_NAS_NUM_FREQ_CH : INTEGER := 64;
     CONSTANT c_NAS_MONO_STEREO : INTEGER := 1;
     CONSTANT c_NAS_POLARITY_TYPE : INTEGER := 2;
 
-    --Inputs
-    SIGNAL i_clock : std_logic := '0';
-    SIGNAL i_reset : std_logic := '0';
-    SIGNAL i_left_avcn_spikes : std_logic_vector(((c_NUM_FREQ_CH*c_NAS_POLARITY_TYPE)-1) DOWNTO 0) := (OTHERS => '0');
-    SIGNAL i_right_avcn_spikes : std_logic_vector(((c_NUM_FREQ_CH*c_NAS_POLARITY_TYPE)-1) DOWNTO 0) := (OTHERS => '0');
-    --Outputs
-    SIGNAL o_ild_out_spikes : std_logic_vector(((c_NUM_FREQ_CH*c_NAS_POLARITY_TYPE)-1) DOWNTO 0);
-
     --Testbench signals
-    SIGNAL tb_start_stimuli : std_logic := '0';
-    SIGNAL tb_end_stimuli : std_logic := '0';
+    SIGNAL tb_start_stimuli : STD_LOGIC := '0';
+    SIGNAL tb_end_stimuli : STD_LOGIC := '0';
 
     --Testbench files
-    FILE tb_input_ts_stimuli_file: text;
-    FILE tb_input_addr_stimuli_file: text;
-    FILE tb_output_aer_events_file: text;
+    FILE tb_input_ts_stimuli_file : text;
+    FILE tb_input_addr_stimuli_file : text;
+    FILE tb_output_aer_events_file : text;
 
 BEGIN
 
@@ -91,7 +130,7 @@ BEGIN
     )
     PORT MAP(
         i_clock => i_clock,
-        i_reset => i_reset,
+        i_nreset => i_nreset,
         i_left_avcn_spikes => i_left_avcn_spikes,
         i_right_avcn_spikes => i_right_avcn_spikes,
         o_ild_out_spikes => o_ild_out_spikes
@@ -99,15 +138,13 @@ BEGIN
 
     --Clock generation
     i_clock <= NOT i_clock AFTER c_i_clock_period/2;
-
-
     --Reset process
     reset_process : PROCESS
     BEGIN
         --Start reset
-        i_reset <= '0';
+        i_nreset <= '0';
         WAIT FOR 1 ms;
-        i_reset <= '1';
+        i_nreset <= '1';
         WAIT FOR 1 ms;
         --Finish reset
 
@@ -181,8 +218,6 @@ BEGIN
         WAIT;
 
     END PROCESS stimuli_process;
-
-
     save_results_process : PROCESS
         VARIABLE v_OLINE : LINE;
         VARIABLE v_sim_time_str : STRING(1 TO 30); -- 30 chars should be enough
@@ -191,7 +226,7 @@ BEGIN
 
         VARIABLE v_event_channel : INTEGER := 0;
         VARIABLE v_event_itd_neuron : INTEGER := 0;
-        
+
     BEGIN
         --// Open RESULTS file in write mode
         file_open(tb_output_aer_events_file, "D:/Proyectos/Universidad/Doctorado/BCBT2018/Projects/Source_location_model_NAS/LSO/Testbench/aerout_test_ild.txt", write_mode);
@@ -212,24 +247,22 @@ BEGIN
             --REPORT "Sim time string.......:'" & v_sim_time_str & "'";
             --REPORT "Event address: " & INTEGER'image(conv_integer(unsigned(aer_data_out)));
 
-            FOR II IN 0 TO ((c_NUM_FREQ_CH*c_NAS_POLARITY_TYPE)-1) LOOP
+            FOR II IN 0 TO ((c_NUM_FREQ_CH * c_NAS_POLARITY_TYPE) - 1) LOOP
                 IF (o_ild_out_spikes(II) = '1') THEN
                     v_event_channel := II;
-        
+
                     REPORT "Output event counter: " & INTEGER'image(v_out_events_counter);
-        
+
                     write(v_OLINE, v_event_channel, right, 1);
                     write(v_OLINE, ',', right, 1);
-        
+
                     write(v_OLINE, v_sim_time_str, right, 1);
-        
+
                     writeline(tb_output_aer_events_file, v_OLINE);
                 ELSE
 
                 END IF;
             END LOOP;
-
-            
         END LOOP;
 
         file_close(tb_output_aer_events_file);
